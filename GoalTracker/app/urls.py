d077429671f730923f18d4446b776c6e51f435ba
Daniel Slick <daniel@danielslick.com>
2016-07-29 16:48:11 -0400

    Modified urls.py to accept additional URLS

--- GoalTracker/app/urls.py
+++ GoalTracker/app/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import include, url
 from . import views
 from django.contrib import admin
 
-urlpatterns = [url(r'^$', views.index, name='index')]
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^app/', include('app.urls')),
+    url(r'^admin/', admin.site.urls),
+]
