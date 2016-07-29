a3407d6bf72c8223ce39a04eef4a96191d52e681
Daniel Slick <daniel@danielslick.com>
2016-07-29 18:45:41 -0400

    Added a db model for Goal and Progress

--- GoalTracker/app/models.py
+++ GoalTracker/app/models.py
@@ -4,4 +4,11 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+class Goal(models.Model):
+    goal_name = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+
+class Progress(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    progress_text = models.CharField(max_length=5)
+    progress_vote = models.IntegerField(default=0)
