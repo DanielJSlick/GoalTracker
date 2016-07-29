2df18b725c54142c5e828c76f3db7fa92e569c28
Daniel Slick <daniel@danielslick.com>
2016-07-29 19:14:19 -0400

    Updated INSTALLED Apps

--- GoalTracker/GoalTracker/settings.py
+++ GoalTracker/GoalTracker/settings.py
@@ -32,8 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app',
-    'app.apps.AppConfig',
+    'app.apps.appConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
